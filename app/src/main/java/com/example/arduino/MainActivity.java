21dba5468ed03e9f3b48106011930f2355dd57e7
user <tkdwls9277@naver.com>
2019-10-12 01:20:37 +0900

    location complete weather test

--- app/src/main/java/com/example/arduino/MainActivity.java
+++ app/src/main/java/com/example/arduino/MainActivity.java
@@ -1,64 +1,166 @@
 package com.example.arduino;
 
-import android.content.ContentValues;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    public static final int THREAD_HANDLER_SUCCESS_INFO = 1;
-    private TextView tv_WeatherInfo;
-
-    private MainActivity mThis;
 
     private TextView tvSolar; // 레이아웃 전환 테스트용
     private TextView tvWater; // 레이아웃 전환 테스트용
 
-    double lat,lon;//위도 경도 값
-    private GpsTracker gt;
+    double lat, lon;//위도 경도 값
+    private GpsInfo gps;
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+
+    TextView Weather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Weather = findViewById(R.id.Weather);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-    private void getWeatherData(double lat, double lng){
-        String url="http://api.openweathermap.org/data/2.5/forecast/daily?lat="
-                +lat+"&lon="+lng+"&units=metric&appid=25101ddb40fe8f611b992f17f1d60b23";
-
+        gps = new GpsInfo(MainActivity.this);
+        lat = gps.getLatitude();
+        lon = gps.getLongitude();
+        callPermission();  // 권한 요청을 해야 함
+        getWeatherData(lat, lon);
     }
 
-    public void onClickView(View v) { // 레이아웃 전환 테스트용
-        switch (v.getId()) {
-            case R.id.textView1:{
-                Intent intent = new Intent(this, SoilActivityTest.class);
-                startActivity(intent);
-            }
-            case R.id.textView2:{
-                Intent intent = new Intent(this, WaterActivityTest.class);
-                startActivity(intent);
-            }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            isAccessFineLocation = true;
+
+        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            isAccessCoarseLocation = true;
+        }
+
+        if (isAccessFineLocation && isAccessCoarseLocation) {
+            isPermission = true;
         }
     }
+
+    // 전화번호 권한 요청
+    private void callPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
+        } else {
+            isPermission = true;
+        }
+    }
+
+
+    private void getWeatherData(double lat, double lng) {
+        String url = "http://api.openweathermap.org/data/2.5/forecast/daily?lat="
+                + lat + "&lon=" + lng + "&units=metric&appid=25101ddb40fe8f611b992f17f1d60b23";
+        new weatherTask().execute();
+
+    }
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById(R.id.loader).setVisibility(View.VISIBLE);
+            findViewById(R.id.mainContainer).setVisibility(View.GONE);
+            findViewById(R.id.errorText).setVisibility(View.GONE);
+            Log.e("onPreExecute","in");
+        }
+
+        protected String doInBackground(String... args) {
+            String url = "http://api.openweathermap.org/data/2.5/forecast/daily?lat="
+                    + lat + "&lon=" + lon + "&units=metric&appid=25101ddb40fe8f611b992f17f1d60b23";
+            Log.e("doInBackground","in");
+            return url;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                Long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+                String pressure = main.getString("pressure");
+                String humidity = main.getString("humidity");
+
+                Long sunrise = sys.getLong("sunrise");
+                Long sunset = sys.getLong("sunset");
+                String windSpeed = wind.getString("speed");
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                /* Populating extracted data into our views */
+                Weather.setText("address = "+address+"temp = "+temp);
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                findViewById(R.id.loader).setVisibility(View.GONE);
+                findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+
+                Log.e("onPostExecute","in");
+
+            } catch (JSONException e) {
+                findViewById(R.id.loader).setVisibility(View.GONE);
+                findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+                Log.e("onPostExecute","catch");
+            }
+
+        }
+    }
+
 }
