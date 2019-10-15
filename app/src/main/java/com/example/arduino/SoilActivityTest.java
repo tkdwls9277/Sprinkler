51f6dcb42e978571f703406e761267cd471d4b7c
tgb1413 <tgb1411@gmail.com>
2019-10-16 06:58:02 +0900

    soil and water activity

--- app/src/main/java/com/example/arduino/SoilActivityTest.java
+++ app/src/main/java/com/example/arduino/SoilActivityTest.java
@@ -3,12 +3,14 @@ package com.example.arduino;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -18,11 +20,14 @@ import java.net.UnknownHostException;
 
 public class SoilActivityTest extends AppCompatActivity {
 
-    private TextView soilValueView;
+    private String serverIP = "117.16.152.128"; // 추후에 변경
+    private int serverPort = 8080; // 추후에 변경
 
+    private TextView soilValueView;
     private TextView connStatusView;
     private TextView ipNumberView;
     private TextView portNumberView;
+    private Button updateBtn;
     private Socket socket;
     private boolean isConnected = false;
 
@@ -35,26 +40,37 @@ public class SoilActivityTest extends AppCompatActivity {
         setContentView(R.layout.activity_soil);
 
         soilValueView = findViewById(R.id.soilValueView);
+        connStatusView = findViewById(R.id.sConnStatusView);
+        ipNumberView = findViewById(R.id.sIpNumberView);
+        portNumberView = findViewById(R.id.sPortNumberView);
+        updateBtn = findViewById(R.id.sUpdateButton);
 
-        connStatusView = findViewById(R.id.connStatusView);
-        ipNumberView = findViewById(R.id.ipNumberView);
-        portNumberView = findViewById(R.id.portNumberView);
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isConnected)
+                    new Thread(new SenderThread("S")).start();
+                else
+                    Toast.makeText(v.getContext(), "Not Connected", Toast.LENGTH_LONG).show();
+            }
+        });
 
-        new Thread(new ConnectThread("192.168.0.7", 8090)).start();
+        new Thread(new ConnectThread(serverIP, serverPort)).start(); // 117.16.152.128
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        isConnected = false;
+        new Thread(new SenderThread("E")).start();
+
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        new Thread(new SenderThread("E")).start();
 
-        isConnected = false;
     }
     private class ConnectThread implements Runnable {
 
@@ -144,7 +160,7 @@ public class SoilActivityTest extends AppCompatActivity {
                     }
 
                     final String recvMessage = bufferedReader.readLine();
-                    Log.e("ReceiverThread", recvMessage);
+                    // Log.e("ReceiverThread", recvMessage);
                     if (recvMessage != null) {
                         runOnUiThread(new Runnable() {
                             @Override
@@ -155,17 +171,18 @@ public class SoilActivityTest extends AppCompatActivity {
                     }
 
                 }
-
+                /*
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     Log.e("ReceiverThread", e.getMessage());
                 }
-
+                */
             }
             catch (IOException e) {
                 Log.e("ReceiverThread", e.getMessage());
             }
+
 
             if (socket != null) {
                 try {
@@ -185,5 +202,33 @@ public class SoilActivityTest extends AppCompatActivity {
             }
         }
 
+    }
+
+    private class SenderThread implements Runnable {
+
+        private String msg;
+
+        public SenderThread (String msg) {
+            this.msg = msg;
+        }
+        @Override
+        public void run() {
+            if (isConnected && socket != null){
+                try {
+                    PrintWriter sendSignal = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8")), true);
+                    sendSignal.println(msg);
+                    sendSignal.flush();
+
+                }
+                catch (IOException e) {
+                    Log.e("SenderThread", e.getMessage());
+                }
+            }
+            else {
+                Log.e("SenderThread", "wtf"); // 뒤로가기 버튼을 누르면 (종료코드) 여기로 온다 왜 그럴까
+            }
+
+            if (msg == "E") isConnected = false; // 종료 코드
+        }
     }
 }
